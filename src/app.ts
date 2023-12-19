3b845edeeaa090bc55833c3ca5c032c3dacc7e9c
Marco Lodovici <mlodovici@alpenite.com>
2023-12-20 00:21:13 +0100

    Add cookie-parser dependency and use it in app.js

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // src/app.js
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import routes from "./routes/routes";
 import { errorHandler } from "./utils/errorHandler";
@@ -9,6 +10,8 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
+
+app.use(cookieParser());
 
 app.use(express.json());
 
