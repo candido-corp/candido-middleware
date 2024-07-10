0c46cc52c6be9ff88d0f549f564fec908bc2e073
Marco Tasca <tascamarcoo@gmail.com>
2024-07-10 15:56:30 +0200

    feat: Add options from external environment in cookie delete function

--- src/utils/cookieDelete.ts
+++ src/utils/cookieDelete.ts
@@ -1,11 +1,20 @@
 import {Response} from "express";
-import {isProduction} from "../config/ConfigApp";
+import {ConfigApp} from "../config/ConfigApp";
 
 export const cookieDelete = (res: Response, cookieName: string) => {
-	res.cookie(cookieName, "", {
+	let defaultHttpOnly: boolean = ConfigApp.cookie.defaultHttpOnly;
+	let defaultSecure: boolean = ConfigApp.cookie.defaultSecure;
+	let defaultSameSite: 'strict' | 'lax' | 'none' = ConfigApp.cookie.defaultSameSite;
+	let defaultPath: string = ConfigApp.cookie.defaultPath;
+	let defaultDomain: string = ConfigApp.cookie.defaultDomain;
+
+	const cookieOptions = {
 		expires: new Date(0),
-		path: "/",
-		httpOnly: false,
-		secure: isProduction,
-	});
+		httpOnly: defaultHttpOnly || false,
+		secure: defaultSecure || false,
+		domain: defaultDomain || "",
+		sameSite: defaultSameSite || "strict",
+		path: defaultPath || "/"
+	};
+	res.cookie(cookieName, null, cookieOptions);
 };
